d954e67d71b17dbc18623e512feb8dc7c1c6dffc
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-03-29 19:20:13 +0530

    Added a config manager and moved config to file.

--- src/kosha/db/pool.clj
+++ src/kosha/db/pool.clj
@@ -1,23 +1,11 @@
 (ns kosha.db.pool
-  (:require [hikari-cp.core :as hikari]
-            [clojure.java.jdbc :as jdbc]))
+  (:require [clojure.java.jdbc :as jdbc]
+            [hikari-cp.core :as hikari]
+            [immuconf.config :as cfg]))
 
-(def datasource-options {:auto-commit        true
-                         :read-only          false
-                         :connection-timeout 30000
-                         :validation-timeout 5000
-                         :idle-timeout       600000
-                         :max-lifetime       1800000
-                         :minimum-idle       10
-                         :maximum-pool-size  10
-                         :pool-name          "db-pool"
-                         :adapter            "postgresql"
-                         :username           "sriharisriraman"
-                         ;;:password           "password"
-                         :database-name      "kosha"
-                         :server-name        "localhost"
-                         :port-number        5432
-                         :register-mbeans    false})
+(defonce config (cfg/load "resources/config.edn"))
+
+(def datasource-options (cfg/get config :database))
 
 (defonce conn
   {:datasource (hikari/make-datasource datasource-options)})
