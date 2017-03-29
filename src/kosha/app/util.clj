ea5a36f27eee78cc4c0285d6500640eb832d2b99
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-03-29 18:50:28 +0530

    Cleaned up the util namespace.

--- src/kosha/app/util.clj
+++ src/kosha/app/util.clj
@@ -1,45 +1,5 @@
 (ns kosha.app.util
-  (:require [clojure.string :as s]
-            [cheshire.core :as json])
-  (:import [java.util.concurrent
-            ThreadPoolExecutor
-            TimeUnit
-            ArrayBlockingQueue
-            ThreadPoolExecutor$CallerRunsPolicy]))
-
-;; public String unaccent(String s) {
-;;     String normalized = Normalizer.normalize(s, Normalizer.Form.NFD);
-;;     return normalized.replaceAll("[^\\p{ASCII}]", "");
-;; }
-
-(defn unaccent [s]
-  (let [normalized (java.text.Normalizer/normalize s java.text.Normalizer$Form/NFD)]
-    (.replaceAll normalized "[^\\p{ASCII}]" "")))
-
-(defn ->printable [swarams & {:keys [bold?]}]
-  (s/join ", " (map (comp s/capitalize name) swarams)))
-
-(defn display-ragam-name [ragam-name]
-  (s/capitalize (name ragam-name)))
-
-(defn create-fixed-threadpool [pool-size queue-size]
-  (ThreadPoolExecutor. pool-size  ;corePoolSize
-                       pool-size  ;maximumPoolSize
-                       60
-                       TimeUnit/SECONDS
-                       (ArrayBlockingQueue. queue-size true)
-                       ;; TODO: ask srihari why we chose this policy -nid/sd
-                       (ThreadPoolExecutor$CallerRunsPolicy.)))
-
-(defn run-in-parallel
-  "Runs tasks parallelly using a fixed threadpool and a bounded ArrayBlockingQueue,
-  where tasks are 0 argument functions."
-  [tasks pool-size queue-size]
-  (let [pool (create-fixed-threadpool pool-size queue-size)
-        rets (mapv #(.get %)
-                   (.invokeAll pool tasks))]
-    (.shutdown pool)
-    rets))
+  (:require [cheshire.core :as json]))
 
 (defn json-response [response]
   {:status 200
