fb234fa0c024ab10fefb14382eeedb2b609782bc
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-03-29 13:09:45 +0530

    Search returns JSON array of ragams and no markup.

--- src/kosha/app/search.clj
+++ src/kosha/app/search.clj
@@ -1,41 +1,15 @@
 (ns kosha.app.search
-  (:require [kosha.app.view :as view]
-            [kosha.db.search :as db-search]
-            [kosha.app.util :as util]
-            [clojure.string :as s]))
+  (:require [kosha.db.search :as db-search]
+            [kosha.app.util :as util]))
 
-(defn external-links [{:keys [k-ragams s-ragams]}
-                      {:keys [raga-link melakartha mela-number]}]
-  [:section.inline
-   (if-not (s/blank? raga-link)
-     [:span.external-link [:a {:href (str "https://en.wikipedia.org" raga-link) :target "blank"} "Wiki"]])
 
-   (if-let [kragam (first k-ragams)]
-     (let [link (format "http://www.karnatik.com/ragas%s.shtml#%s" (first kragam) kragam)]
-       [:span.external-link [:a {:href link :target "blank"} "Karnatik"]]))
+(defn get-ragam
+  "Retrieves 10 best matches for given ragam name."
+  [{:keys [params] :as request}]
+  (let [ragams (db-search/ragams (:q params) 10)]
+    (util/json-response ragams)))
 
-   (if-let [sragam (first s-ragams)]
-     (let [link "http://www.sangeethapriya.org/fetch_tracks.php?ragam"]
-       [:form.inline.external-link {:action link :method "POST" :target "blank"}
-        [:input {:type :hidden :name "FIELD_TYPE" :value sragam}]
-        [:input {:type :submit :name "Sangeethapriya" :value "Sangeethapriya"}]]))])
-
-(defn mela-data [{:keys [melakartha mela-number] :as metadata}]
-  (if melakartha
-    (format "This is mela %s" mela-number)
-    (format "Janya of %s" mela-number)))
-
-(defn ragam [{:keys [params] :as request}]
-  (let [ragams (db-search/ragams-with-scales (:q params) 10)]
-    {:status 200
-     :body (view/html-skeleton
-            [:table
-             [:thead [:th "Ragam"] [:th "Arohanam"] [:th "Avarohanam"] [:th "Mela"] [:th "Links"]]
-             [:tbody
-              (for [{:keys [ragam-name arohanam avarohanam metadata] :as ragam} ragams]
-                [:tr
-                 [:td (util/unaccent ragam-name)]
-                 [:td arohanam]
-                 [:td avarohanam]
-                 [:td (mela-data metadata)]
-                 [:td (external-links ragam metadata)]])]])}))
+(defn handler [{:keys [params] :as request}]
+  (let [type (get-in request [:params :t])]
+    (case type
+      "ragam" (get-ragam request))))
