134a29b0a8fc9f0b9c8196d6f8cf925b424a3739
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-04-19 21:14:05 +0530

    Parse lyrics of kriti from hstore to clojure map.

--- src/kosha/db/kriti.clj
+++ src/kosha/db/kriti.clj
@@ -1,5 +1,33 @@
 (ns kosha.db.kriti
-  (:require [kosha.db.util :as db-util]))
+  (:require [clojure.set :as set]
+            [clojure.string :as str]
+            [kosha.db.util :as db-util]
+            [pg-hstore.core :as hs]))
+
+(defn to-kebab-case
+  "Takes a keyword with spaces and converts it to a kebab-cased keyword."
+  [key-with-spaces]
+  (let [key-string         (name key-with-spaces)
+        kebab-cased-string (str/replace key-string #" " "-")]
+    (keyword kebab-cased-string)))
+
+(defn rename-stanza-keys
+  "Renames keys of stanzas to replace spaces in them, if the lyrics has named stanzas."
+  [{:keys [content has-named-stanzas] :as lyrics}]
+  (let [lyrics-map   (hs/from-hstore content)
+        stanza-keys  (keys lyrics-map)
+        new-names    (into {} (map (juxt identity to-kebab-case) stanza-keys))]
+    (if has-named-stanzas
+      (assoc lyrics :content (set/rename-keys (hs/from-hstore content) new-names))
+      lyrics)))
+
+(defn parse-lyrics
+  "Parses lyrics of a kriti into a clojure map if it has named stanzas."
+  [lyrics]
+  (-> lyrics
+      (hs/from-hstore)
+      (update :has-named-stanzas (partial = "true"))
+      (rename-stanza-keys)))
 
 (defn kriti
   "Retrieve details of a kriti given it's kriti_id."
@@ -9,7 +37,8 @@
             JOIN ragams r
             ON k.kriti_id = ? AND r.ragam_id = k.ragam_id; "
            kriti-id]]
-    (first (db-util/run-query q))))
+    (-> (first (db-util/run-query q))
+        (update :lyrics parse-lyrics))))
 
 (defn renditions-of-kriti
   "Retrieve all renditions of a kriti."
