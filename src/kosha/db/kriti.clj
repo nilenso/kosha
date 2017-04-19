4cbd049132564e78ff5bcbbb9b073aa9c5ebaf23
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-04-20 00:25:59 +0530

    Move lyrics parsing logic to app.kriti ns.

--- src/kosha/db/kriti.clj
+++ src/kosha/db/kriti.clj
@@ -1,53 +1,5 @@
 (ns kosha.db.kriti
-  (:require [clojure.set :as set]
-            [clojure.string :as str]
-            [kosha.db.util :as db-util]
-            [pg-hstore.core :as hs]))
-
-(defn to-kebab-case
-  "Takes a keyword with spaces and converts it to a kebab-cased keyword."
-  [key-with-spaces]
-  (let [key-string         (name key-with-spaces)
-        kebab-cased-string (str/replace key-string #" " "-")]
-    (keyword kebab-cased-string)))
-
-(defn rename-stanza-keys
-  "Renames keys of stanzas to replace spaces in them, if the lyrics has named stanzas."
-  [content]
-  (let [lyrics-map   (hs/from-hstore content)
-        stanza-keys  (keys lyrics-map)
-        new-names    (into {} (map (juxt identity to-kebab-case) stanza-keys))]
-    (set/rename-keys (hs/from-hstore content) new-names)))
-
-(defn remove-keys
-  "Removes all keys in the given collection from the given map."
-  [map keys-coll]
-  (apply dissoc map keys-coll))
-
-(defn parse-charanams
-  "Parses charanams into a vector under :charanams key if the lyrics has named stanzas"
-  [content]
-  (let [max-charanams          (count (keys content))
-        charanam-key-names     (map #(str "caraNam-" %) (range 1 max-charanams))
-        possible-charanam-keys (concat [:caraNam] (map keyword charanam-key-names))
-        charanams              (into [] (vals (select-keys content possible-charanam-keys)))]
-    (-> content
-        (assoc :charanams charanams)
-        (remove-keys possible-charanam-keys))))
-
-(defn parse-lyrics
-  "Parses lyrics of a kriti into a clojure map if it has named stanzas."
-  [lyrics]
-  (as-> lyrics lyr
-    (when lyr
-      (-> lyr
-          (hs/from-hstore)
-          (update :has-named-stanzas (partial = "true"))))
-    (if (:has-named-stanzas lyr)
-      (-> lyr
-          (update :content rename-stanza-keys)
-          (update :content parse-charanams))
-      lyr)))
+  (:require [kosha.db.util :as db-util]))
 
 (defn kriti
   "Retrieve details of a kriti given it's kriti_id."
@@ -57,8 +9,7 @@
             JOIN ragams r
             ON k.kriti_id = ? AND r.ragam_id = k.ragam_id; "
            kriti-id]]
-    (-> (first (db-util/run-query q))
-        (update :lyrics parse-lyrics))))
+    (first (db-util/run-query q))))
 
 (defn renditions-of-kriti
   "Retrieve all renditions of a kriti."
