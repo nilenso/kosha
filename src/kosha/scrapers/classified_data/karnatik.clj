ee30a8dd623340a9c43cb31b9b52391322bb80ff
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-05-04 21:21:00 +0530

    Refactor scraper for test data (+ srihari).

--- src/kosha/scrapers/classified_data/karnatik.clj
+++ src/kosha/scrapers/classified_data/karnatik.clj
@@ -2,7 +2,8 @@
   (:require [clojure.edn :as edn]
             [clojure.pprint :as pp]
             [clojure.string :as str]
-            [kosha.scrapers.util :as util])
+            [kosha.scrapers.util :as util]
+            [medley.core :as m])
   (:import org.jsoup.Jsoup
            [org.jsoup.nodes Document Element]
            org.jsoup.select.Elements))
@@ -13,9 +14,6 @@
 (defn karnatik-url
   [letter]
   (str "http://www.karnatik.com/ragas" letter ".shtml"))
-
-(defn char-range [start end]
-  (map char (range (int start) (inc (int end)))))
 
 (def karnatik-chars
   ["a" "b" "c" "d" "e" "g" "h" "i" "j" "k" "l" "m" "n" "o" "p" "r" "s" "t" "u" "v" "w" "y"])
@@ -28,6 +26,9 @@
   [jsoup-anchor-tag]
   (not (empty? (.children jsoup-anchor-tag))))
 
+(def annotate-children
+  (juxt ragam-name has-children?))
+
 (defn ragam-anchor-tags
   "Call the url to scrape, and obtain the data of interest, i.e. ragam names."
   [url]
@@ -36,62 +37,40 @@
       (.select "td[width=420]")
       (.select "font")
       (.select "font")
-      (.select "blockquote")
-      (.select "a[name]")))
+      (.select "blockquote > a[name]")))
 
-(defn add-separators
-  "Reducing function that adds nil value in between different ragas in vector."
-  [acc elem]
-  (cond (empty? acc) (conj acc elem)
-        (second elem) (conj acc nil elem)
-        (not (second elem)) (conj acc elem)))
-
-(defn ragam-name-vectors
-  "Convert each jsoup anchor tag in argument to ['ragamname', boolean] pair."
-  [ragam-anchor-tags]
-  (->> ragam-anchor-tags
-       (map (juxt ragam-name has-children?))
-       (vec)))
-
-(defn grouped-ragams
-  "Take a vector of ragam vectors, and group the vectors if they are synonyms for the same ragam."
-  [ragam-vectors]
-  (->> ragam-vectors
-       (reduce add-separators [])
-       (partition-by nil?)
-       (remove #(nil? (first %)))))
+(defn group-ragams [annotated-ragam-names]
+  (reduce (fn [acc [ragam-name has-children?]]
+            (let [created-groups (vec (butlast acc))
+                  current-ragam-group (conj (last acc) ragam-name)]
+              (if has-children?
+                (conj created-groups current-ragam-group #{})
+                (conj created-groups current-ragam-group))))
+          [#{}]
+          annotated-ragam-names))
 
 (defn ragam-name-sets
   [letter]
   (->> (ragam-anchor-tags (karnatik-url letter))
-       (ragam-name-vectors)
-       (rseq)
-       (grouped-ragams)))
+       (mapv annotate-children)
+       (group-ragams)))
 
-(defn synonyms
-  "Given a vector of grouped ragams as ['ragam-name', boolean] pairs, convert to kv, where v is the set of synonyms."
-  [name-sets]
-  (mapcat seq
-          (for [name-set name-sets]
-            (let [name (first (first (filter second name-set)))
-                  synonym-names (apply hash-set (map first name-set))]
-              [name synonym-names]))))
+(defn key-by-first [coll]
+  (->> coll
+       (group-by first)
+       (m/map-vals first)))
 
-(defn get-all-classified-ragams
-  "Return a map where keys are beginning letters, and values are maps of ragam-name->synonyms."
-  []
-  (into {}
-        (vec (for [char karnatik-chars]
-               (let [syns (synonyms (ragam-name-sets char))]
-                 (apply hash-map syns))))))
+(defn get-all-classified-ragams []
+  (->> karnatik-chars
+       (mapcat ragam-name-sets)
+       key-by-first))
 
-(defn get-all-ragam-names
-  []
-  (flatten (for [letter karnatik-chars]
-             (->> (karnatik-url letter)
-                  ragam-anchor-tags
-                  (map ragam-name)
-                  (vec)))))
+(defn get-all-ragam-names []
+  (->>  karnatik-chars
+        (map karnatik-url)
+        (mapcat ragam-anchor-tags)
+        (map ragam-name)))
+
 
 (defn write-to-file
   []
