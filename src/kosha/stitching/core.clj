7f6f094f246da93e32d8a01fd283346299dcf55d
Shaaz Ahmed <shaaz.ahmed@hotmail.com>
2017-05-04 12:22:18 +0530

    Add ability to combine string metrics and use arbitrary conditions, using stencil templates.

--- src/kosha/stitching/core.clj
+++ src/kosha/stitching/core.clj
@@ -3,7 +3,8 @@
             [kosha.scrapers.classified-data.karnatik-reader :as data]
             [kosha.stitching.scoring :as score]
             [kosha.stitching.util :as util]
-            [loom.alg :as graph]))
+            [loom.alg :as graph]
+            [stencil.core :as stencil]))
 
 (defn create-names-table
   "Creates a new table with (id, name) and returns a map of id-column name, name-column name, table-name."
@@ -32,9 +33,10 @@
 
 (defn get-edges-by-id
   "Gets the id->id edge list of the similarity graph, given a similarity function and a min threshold value."
-  [table similarity-fn-name threshold]
-  (let [q ["SELECT source, target FROM similarity_graph_id(?::text, ?::text, ?, ?);"
-           (:name table) similarity-fn-name threshold comparator]
+  [table condition]
+  (let [template-q (str "SELECT {{a}}_id AS source, {{b}}_id AS target FROM {{table}} r1 JOIN {{table}} r2 ON {{a}} != {{b}} AND "
+                        condition ";")
+        q (stencil/render-string template-q {:a "r1.name" :b "r2.name" :table (:name table)})
         edges (db-util/run-query q)]
     (set (map #(hash-set (:source %) (:target %)) edges))))
 
@@ -47,9 +49,10 @@
 
 (defn get-edges-by-string
   "Gets the string->string edge list of the similarity graph, given a similarity function and a min threshold value."
-  [table similarity-fn-name comparator threshold]
-  (let [q ["SELECT source, target FROM similarity_graph_string(?::text, ?::text, ?, ?);"
-           (:name table) similarity-fn-name threshold comparator]
+  [table condition]
+  (let [template-q (str "SELECT {{a}} AS source, {{b}} AS target FROM {{table}} r1 JOIN {{table}} r2 ON {{a}} != {{b}} AND "
+                        condition ";")
+        q (stencil/render-string template-q {:a "r1.name" :b "r2.name" :table (:name table)})
         edges (db-util/run-query q)]
     (set (map #(hash-set (:source %) (:target %)) edges))))
 
@@ -62,21 +65,22 @@
 (defonce *classified-ragams (data/read-scraped "output/classified-test-data.edn"))
 (defonce *expected-edges (data/edge-list *classified-ragams))
 
-(defn score-strategy [f comparator threshold]
-  (let [s (get-edges-by-string {:name "all_ragam_names"} f comparator threshold)]
+(defn score-strategy [condition]
+  (let [s (get-edges-by-string {:name "all_ragam_names"} condition)]
     (def *s s)
     (score/compare-edge-lists *expected-edges s)))
 
 (defn score-all-strategies []
-  {:similarity-0.8 (score-strategy "similarity" ">" 0.8)
-   :similarity-0.75 (score-strategy "similarity" ">" 0.75)
-   :similarity-0.7 (score-strategy "similarity" ">" 0.7)
-   :similarity-0.675 (score-strategy "similarity" ">" 0.675)
-   :similarity-0.65 (score-strategy "similarity" ">" 0.65)
-   :similarity-0.625 (score-strategy "similarity" ">" 0.625)
-   :similarity-0.6 (score-strategy "similarity" ">" 0.6)
-   :levenshtein-1 (score-strategy "levenshtein" "<=" 1)
-   :soundex (score-strategy "difference" ">" 3)})
+  {:similarity-0.8 (score-strategy "similarity({{a}}, {{b}}) > 0.8")
+   :similarity-0.75 (score-strategy "similarity({{a}}, {{b}}) > 0.75")
+   :similarity-0.7 (score-strategy "similarity({{a}}, {{b}}) > 0.7")
+   :similarity-0.675 (score-strategy "similarity({{a}}, {{b}}) > 0.675")
+   :similarity-0.65 (score-strategy "similarity({{a}}, {{b}}) > 0.65")
+   :similarity-0.625 (score-strategy "similarity({{a}}, {{b}}) > 0.625")
+   :similarity-0.6 (score-strategy "similarity({{a}}, {{b}}) > 0.6")
+   :levenshtein-1 (score-strategy "levenshtein({{a}}, {{b}}) <= 1")
+   :soundex (score-strategy "difference({{a}}, {{b}}) > 3")
+   :similarity-lev (score-strategy "similarity({{a}}, {{b}}) + (1 - levenshtein({{a}}, {{b}})/char_length({{a}})) > 1.6")})
 
 
 
